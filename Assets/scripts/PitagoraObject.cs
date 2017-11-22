371588560dd3447fe4b2389934b77b5dcc0f4a60
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-22 16:06:57 +0900

    [add] 画面内のオブジェクトをドラッグできるようにした

---
+++ Assets/scripts/PitagoraObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PitagoraObject : MonoBehaviour
+{
+  void OnMouseDrag()
+  {
+    Vector3 screenPos = Input.mousePosition;
+    Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+    worldPos.z = 0;
+    transform.position = worldPos;
+  }
+}
