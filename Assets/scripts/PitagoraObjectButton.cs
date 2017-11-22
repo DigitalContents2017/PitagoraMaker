1dbd11bcf460d935c355c54957b8afd56d0546ff
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-23 01:23:30 +0900

    [add] オブジェクトを配置できるようにした

---
+++ Assets/scripts/PitagoraObjectButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PitagoraObjectButton : MonoBehaviour, IPointerDownHandler
+{
+  public GameObject PitagoraObject;
+
+  void Start()
+  {
+    CreateObject();
+  }
+
+  public void OnPointerDown (PointerEventData eventData)
+  {
+    CreateObject();
+  }
+
+  void CreateObject()
+  {
+    Vector3 position = transform.position;
+    Instantiate(PitagoraObject, position, Quaternion.identity);
+  }
+}
