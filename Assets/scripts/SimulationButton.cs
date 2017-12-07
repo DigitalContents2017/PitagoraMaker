1117f84552051b2d28354ca1dfec4c81c76bdaa7
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-12-07 14:36:40 +0900

    [add] シミュレーションの開始・終了を実装

---
+++ Assets/scripts/SimulationButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class SimulationButton : MonoBehaviour {
+  public void OnClick()
+  {
+    SimulationManager.SwitchSimulation();
+  }
+}
---
+++ Assets/scripts/SimulationManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SimulationManager : MonoBehaviour {
+  static bool isSimulating = false;
+
+  public static void SwitchSimulation() {
+    if(isSimulating) {
+      EndSimulation();
+    } else {
+      StartSimulation();
+    }
+  }
+
+  static void StartSimulation()
+  {
+    Debug.Log("start");
+    isSimulating = true;
+  }
+
+  static void EndSimulation()
+  {
+    Debug.Log("end");
+    isSimulating = false;
+  }
+}
