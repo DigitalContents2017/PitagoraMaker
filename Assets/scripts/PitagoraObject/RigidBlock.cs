60c41f33d0fd8393859a229f9b3e410efeff1241
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-12-11 02:28:23 +0900

    [add] シュミレーションが終わったら元の位置に戻す

--- Assets/scripts/PitagoraObject/RigidBlock.cs
+++ Assets/scripts/PitagoraObject/RigidBlock.cs
@@ -4,6 +4,8 @@ using System.Collections;
 class RigidBlock : Block
 {
   Rigidbody2D rigidbody;
+  Vector2 savedPosition;
+  Quaternion savedRotation;
 
 	void Start()
 	{
@@ -13,6 +15,9 @@ class RigidBlock : Block
 
   public override void StartSimulation()
   {
+    savedPosition = new Vector2(transform.position.x, transform.position.y);
+    savedRotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+
     if(!isButton)
     {
       rigidbody.constraints = RigidbodyConstraints2D.None;
@@ -22,5 +27,7 @@ class RigidBlock : Block
   public override void EndSimulation()
   {
     rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+    transform.position = savedPosition;
+    transform.rotation = savedRotation;
   }
 }
