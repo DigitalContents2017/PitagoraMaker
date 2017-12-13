f0f40c261f2579874bf250be8f06815eb7e80f6c
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-12-14 03:14:44 +0900

    [add] GoalしたらGoalScreenを表示するようにした

--- Assets/scripts/PitagoraObject/Goal.cs
+++ Assets/scripts/PitagoraObject/Goal.cs
@@ -1,12 +1,26 @@
 using System;
 using UnityEngine;
 
-class Goal : MonoBehaviour {
+class Goal : PitagoraObject {
+	Canvas goalScreen;
+	bool isSimulating = false;
+
 	void Start() {
 		StageManager.SetObject(transform.position);
+		goalScreen = GameObject.Find("GoalScreen").GetComponent<Canvas>();
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		Debug.Log("Hit");
+		if (isSimulating) {
+			goalScreen.enabled = true;
+		}
+	}
+
+	public override void StartSimulation() {
+		isSimulating = true;
+	}
+
+	public override void EndSimulation() {
+		isSimulating = false;
 	}
 }
