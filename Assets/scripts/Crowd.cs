d816ea838b12a74c62a500275aab46f0d4f647cb
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-12-16 00:24:41 +0900

    [add] 雲が動くようにした

---
+++ Assets/scripts/Crowd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = transform.localPosition;
+		pos.x -= 0.002f / transform.localScale.x;
+		if(pos.x < -22) {
+			pos.x = 22;
+		}
+		transform.localPosition = pos;
+	}
+}
