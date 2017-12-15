c00f75f3516968fdcdbe91b1d5faaea7ce237ce8
Kazuki Otao <kaootao@gmail.com>
2017-12-16 02:36:20 +0900

    Goldが動く問題

--- Assets/scripts/PitagoraObject/PitagoraObject.cs
+++ Assets/scripts/PitagoraObject/PitagoraObject.cs
@@ -84,10 +84,12 @@ public class PitagoraObject : MonoBehaviour {
 	}
 
 	void OnMouseDrag() {
-		Vector3 screenPos = Input.mousePosition;
-		Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-		worldPos.z = 0;
-		this.transform.position = worldPos;
+		if(this.IsHold) {
+			Vector3 screenPos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+			worldPos.z = 0;
+			this.transform.position = worldPos;
+		}
 	}
 
 	void OnMouseUp() {
